e83d83b1158d022802604db313620254fbf7f12f
Bryan <bryanpiga1309@gmail.com>
2026-01-19 19:39:43 -0600

    Se termina el core del frontend

--- frontend/src/app/core/services/user/user.service.ts
+++ frontend/src/app/core/services/user/user.service.ts
@@ -1,11 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment.development';
+import { User } from '../../types/User';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private baseUrl=`${environment.BACK_URL}/`
+  private baseUrl=`${environment.BACK_URL}/users`;
+
   constructor(private httpClient:HttpClient) { }
+
 }
